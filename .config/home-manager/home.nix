c5fa2cce6866302c0a1109211ba7f3ba69933eab
Darrick Ross <me@darrickross.com>
2025-07-27 11:30:56 -0400

    Let Nix manage my `.bashrc` but inject my `.bashrc` so I can inject packages through nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -18,27 +18,30 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    pkgs.bat
+    pkgs.eza
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = { ".bashrc".source = ../../.bashrc; };
+  # home.file = { ".bashrc".source = ../../.bashrc; };
+
+  programs.bash.enable = true;
+
+  programs.bash.bashrcExtra = ''
+    source ${../../.bashrc}
+  '';
+
+  programs.bash.shellAliases = {
+    cat = "bat";
+    ls = "eza";
+  };
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
