268428d4aafcebc97f32f03ee0c328147a422cfd
Darrick Ross <me@darrickross.com>
2025-08-13 21:55:06 -0400

    switch to `with pkgs` instead of `pkgs.<name>`

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -17,29 +17,29 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    pkgs.bat
-    pkgs.eza
-    pkgs.jq
-    pkgs.gh # github cli
-    pkgs.nixfmt-rfc-style # vscode plugin: jnoortheen.nix-ide
-    pkgs.oh-my-posh
-    (pkgs.python313.withPackages (py: [
+    bat
+    eza
+    jq
+    gh # github cli
+    nixfmt-rfc-style # vscode plugin: jnoortheen.nix-ide
+    oh-my-posh
+    (python313.withPackages (py: [
       py.ansible-core # vscode plugin: redhat.ansible
       py.black # vscode plugin: ms-python.black-formatter
       py.cfn-lint # vscode plugin: kddejong.vscode-cfn-lint
       py.pillow # dotfiles/scripts/pdf/convert_tif_jpg_to_pdf.py
       py.pypdf2 # dotfiles/scripts/pdf/<multiple>
     ]))
-    pkgs.shellcheck # vscode plugin: timonwong.shellcheck
-    pkgs.shfmt # vscode plugin: foxundermoon.shell-format
-    pkgs.yq
+    shellcheck # vscode plugin: timonwong.shellcheck
+    shfmt # vscode plugin: foxundermoon.shell-format
+    yq
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
